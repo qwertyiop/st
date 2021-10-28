10c3ff4b88c7678964200278d736d93d9af252fa
Стас <stanislav84@gmx.com>
2021-10-28 20:46:33 +0600

    default geometry set to 124x29

--- config.def.h
+++ config.def.h
@@ -157,8 +157,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 124;
+static unsigned int rows = 29;
 
 /*
  * Default colour and shape of the mouse cursor
