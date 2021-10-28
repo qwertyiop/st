4d261bebbd300f8922c2f7e9d2beb3e6a92c8131
Стас <stanislav84@gmx.com>
2021-10-28 20:50:38 +0600

    FG, BG and Cursor colors changed to better accomodate shell color theme

--- config.def.h
+++ config.def.h
@@ -139,9 +139,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 7;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 255;
 static unsigned int defaultrcs = 257;
 
 /*
